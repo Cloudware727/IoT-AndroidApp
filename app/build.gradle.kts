9b9346c3de0d8543344859d38e6dd7cb4edd717e
Shlok Patel <shlokkrunalkumar.patel@student.kuleuven.be>
2025-03-15 09:51:05 +0100

    undo coffee select

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    // Material3 (Latest version - check for updates)
+    implementation("com.google.android.material:material:1.12.0")
+
+    // ViewPager2
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
 }
